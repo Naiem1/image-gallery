3f4e645039b325fc28da6a4ff44f423c625e17b2
Naiem1 <naiem4288@gmail.com>
2023-11-03 23:21:30 +0600

    added hover effect

--- src/components/done/Image.tsx
+++ src/components/done/Image.tsx
@@ -1,5 +1,8 @@
-import { Box } from '@mui/material';
+import { Button } from '@mui/material';
+import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
 import { forwardRef } from 'react';
+import '../../assets/styles.css';
 
 // interface Props {
 //   url: {
@@ -12,7 +15,7 @@ import { forwardRef } from 'react';
 // }
 
 const Image = forwardRef(({ url, index, faded, style, ...props }, ref) => {
-  // console.log('Photo', props)
+  console.log('Photo', props);
   const imageStyle = {
     opacity: faded ? '0.2' : '1',
     transformationOrigin: '0, 0',
@@ -24,9 +27,42 @@ const Image = forwardRef(({ url, index, faded, style, ...props }, ref) => {
     backgroundPosition: 'center',
     borderRadius: '5px',
     border: '1px solid rgb(204, 207, 210)',
+    position: 'relative',
+    zIndex: 1,
     ...style,
   };
-  return <Box ref={ref} style={imageStyle} {...props} />;
+  return (
+    <>
+      <Box ref={ref} style={imageStyle} {...props} className="image">
+        <Box
+          className="bg-black"
+          sx={{
+            width: '100%',
+            height: '100%',
+            background: 'rgba(188, 187, 187, 0.353)',
+            position: 'relative',
+            zIndex: 9000,
+            paddingLeft: '15px',
+            paddingTop: '5px',
+          }}
+        >
+          <Checkbox sx={{ alignSelf: 'start' }} defaultChecked />
+          <Button
+            variant="outlined"
+            sx={{
+              border: '1px solid black',
+              color: 'black',
+              position: 'absolute',
+              left: '40%',
+              top: '40%',
+            }}
+          >
+            view
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
 });
 
 export default Image;
